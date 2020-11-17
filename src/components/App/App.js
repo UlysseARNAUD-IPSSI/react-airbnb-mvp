fe01810d5ade87d951986fa90f355e84a8237937
Ulysse ARNAUD <mexanga@gmail.com>
2020-11-17 11:52:28 +0100

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import logo from '../../assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Coucou !
+      </header>
+    </div>
+  );
+}
+
+export default App;
