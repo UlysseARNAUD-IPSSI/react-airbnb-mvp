1b30a694614ca2a1f798ba42c66db51aa28d74fc
Ulysse ARNAUD <mexanga@gmail.com>
2020-11-18 09:55:17 +0100

    Ajout des étoiles pour le rang des appartements

--- src/components/ApartmentStars/index.js
+++ src/components/ApartmentStars/index.js
@@ -1,9 +1,19 @@
 // import logo from '../../assets/logo.svg';
 import './index.css';
 
-function ApartmentStars() {
+function ApartmentStars(props) {
+    console.log('ApartmentStars constructor')
+
+    const {rank} = props
+
+    const content = !!rank ? "★★★★★" : ""
+
+    const style = {
+        "--rating": rank ?? "100"
+    }
+
     return (
-        <div className="apartment-stars"></div>
+        <div className="apartment-stars" style={style}>{content}</div>
     )
 }
 
